19eef479afff03eeb5c1b144e4c744cf93cf4fc1
João Paulo <musicjoaopaulo@gmail.com>
2020-03-11 20:23:19 -0300

    chore(backend): create loader models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Joao Paulo',
+    email: 'jpfricks@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
