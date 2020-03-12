0f7edff45d824e4437061d68c6c2671f726e7a5b
João Paulo <musicjoaopaulo@gmail.com>
2020-03-11 21:01:12 -0300

    chore(backend): jwt auth

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -5,7 +5,7 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ error: 'User already exists' });
+      return res.status(400).json({ error: 'User already exists.' });
     }
 
     const { id, name, email } = await User.create(req.body);
