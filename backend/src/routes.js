01a8508396a7531e8fa976eee9fb9460cb3f1718
João Paulo <musicjoaopaulo@gmail.com>
2020-03-12 12:54:24 -0300

    feat(backend): crud of recipients

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,19 +5,26 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authMiddleware);
 
+routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/recipients', RecipientController.index);
+routes.get('/recipients/:id', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
+routes.delete('/recipients/:id', RecipientController.delete);
 
 export default routes;
