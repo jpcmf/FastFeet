964148aa1579acf5a31c9d24d0c614dd822e8156
João Paulo <musicjoaopaulo@gmail.com>
2020-03-12 09:15:31 -0300

    chore(backend): configure environments variables

--- backend/src/app/models/File.js
+++ backend/src/app/models/File.js
@@ -6,6 +6,12 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.VIRTUAL,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `${process.env.APP_URL}/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
