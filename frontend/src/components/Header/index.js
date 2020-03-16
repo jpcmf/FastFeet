54e3954ce7798da0f33cc2ab0c70649bae0bab27
João Paulo <musicjoaopaulo@gmail.com>
2020-03-16 20:57:52 -0300

    chore: create header component

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Content, Profile } from './styles';
+
+import { signOut } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.png';
 
 export default function Header() {
-  return <h1>Header</h1>;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <Link className="logo" to="/">
+            <img src={logo} alt="FastFeet" />
+          </Link>
+          <ul>
+            <NavLink activeStyle={{ color: '#444' }} id="orders" to="/orders">
+              ENCOMENDAS
+            </NavLink>
+            <NavLink
+              activeStyle={{ color: '#444' }}
+              id="deliverymen"
+              to="/deliverymen"
+            >
+              ENTREGADORES
+            </NavLink>
+            <NavLink
+              activeStyle={{ color: '#444' }}
+              id="recipients"
+              to="/recipients"
+            >
+              DESTINATÁRIOS
+            </NavLink>
+            <NavLink
+              activeStyle={{ color: '#444' }}
+              id="order-problems"
+              to="/order-problems"
+            >
+              PROBLEMAS
+            </NavLink>
+          </ul>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <NavLink to="/" onClick={handleSignOut}>
+                sair do sistema
+              </NavLink>
+            </div>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
 }
--- frontend/src/store/modules/user/reducer.js
+++ frontend/src/store/modules/user/reducer.js
@@ -1,8 +1,21 @@
-const INITIAL_STATE = {};
+import produce from 'immer';
 
-export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+const INITIAL_STATE = {
+  profile: null,
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.profile = action.payload.user;
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.profile = null;
+        break;
+      }
+      default:
+    }
+  });
 }
