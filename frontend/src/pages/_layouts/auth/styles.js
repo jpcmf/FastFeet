aaa85204e87b9197c1a421975b9d94f136d72030
João Paulo <musicjoaopaulo@gmail.com>
2020-03-16 20:20:58 -0300

    feat: create the signin

---
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #7d40e7;
+  height: 100%;
+`;
--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,3 +1,51 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    console.tron.log(response);
+
+    const { token, user } = response.data;
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/orders');
+  } catch (error) {
+    toast.error('Falha na autenticação. Usuário e senha não conferem.');
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
