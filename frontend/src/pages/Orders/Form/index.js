ff6d95646771772fb6214d67e278f004793f839a
João Paulo <musicjoaopaulo@gmail.com>
2020-03-17 18:41:10 -0300

    feat: create crud of orders

---
+++ frontend/src/pages/Orders/Form/index.js
@@ -0,0 +1,180 @@
+import React, { useState } from 'react';
+
+import { MdKeyboardArrowLeft, MdCheck } from 'react-icons/md';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { Form, Input } from '@rocketseat/unform';
+
+import {
+  Container,
+  Header,
+  StyledButton,
+  Content,
+  CustomAsyncSelect,
+} from './styles';
+
+import history from '~/services/history';
+import api from '~/services/api';
+
+export default function OrdersForm({ history: navigation }) {
+  const { order } = navigation.location.state || '';
+  const [loading, setLoading] = useState(false);
+  const [recipient, setRecipient] = useState(order ? order.recipient : '');
+  const [deliveryman, setDeliveryman] = useState(
+    order ? order.deliveryman : ''
+  );
+
+  async function loadRecipients(q) {
+    const response = await api.get('/recipients', { params: { q } });
+
+    return new Promise(resolve => {
+      resolve(
+        response.data.rows.map(st => {
+          return {
+            value: st.id,
+            label: st.name,
+            ...st,
+          };
+        })
+      );
+    });
+  }
+
+  async function loadDeliverymen(q) {
+    const response = await api.get('/deliverymen', { params: { q } });
+
+    return new Promise(resolve => {
+      resolve(
+        response.data.rows.map(st => {
+          return {
+            value: st.id,
+            label: st.name,
+            ...st,
+          };
+        })
+      );
+    });
+  }
+
+  async function handleSubmit(o) {
+    let errors = 0;
+
+    if (recipient === null || recipient === '') {
+      toast.error('O destinatário precisa ser selecionando.');
+    }
+
+    if (deliveryman === null || deliveryman === '') {
+      toast.error('O entregador precisa ser selecionando.');
+    }
+
+    if (o.product === '') {
+      toast.error('O produto precisa ser preenchido.');
+      errors += 1;
+    }
+
+    if (errors >= 1) return;
+
+    if (order) {
+      try {
+        setLoading(true);
+
+        await api.put(`/orders/${order.id}`, {
+          recipient_id: recipient.id,
+          deliveryman_id: deliveryman.id,
+          product: o.product,
+        });
+
+        setLoading(false);
+
+        toast.success('A encomenda foi editada com sucesso.');
+      } catch (error) {
+        setLoading(false);
+
+        toast.error('Erro ao editar encomenda.');
+      }
+    } else {
+      try {
+        setLoading(true);
+
+        await api.post(`/orders`, {
+          recipient_id: recipient.id,
+          deliveryman_id: deliveryman.id,
+          product: o.product,
+        });
+
+        setLoading(false);
+
+        toast.success('A encomenda foi cadastrada com sucesso.');
+
+        history.push('/orders');
+      } catch (error) {
+        setLoading(false);
+
+        toast.error('Erro ao cadastrar encomenda.');
+      }
+    }
+  }
+  return (
+    <Container>
+      <Form initialData={order} onSubmit={handleSubmit}>
+        <Header>
+          <p>{order ? 'Edição' : 'Cadastro'} de encomendas</p>
+          <div className="headerButtons">
+            <StyledButton onClick={() => history.push('/orders')} type="button">
+              <MdKeyboardArrowLeft size={20} color="#fff" />
+              VOLTAR
+            </StyledButton>
+            <StyledButton type="submit">
+              <MdCheck size={20} color="#fff" />
+              {loading ? 'SALVANDO...' : 'SALVAR'}
+            </StyledButton>
+          </div>
+        </Header>
+
+        <Content>
+          <div className="searchInputs">
+            <div>
+              <span>Destinatário</span>
+              <CustomAsyncSelect
+                isSearchable
+                defaultOptions
+                loadOptions={e => loadRecipients(e)}
+                defaultValue={{
+                  label: order ? order.recipient.name : '',
+                  value: order ? order.recipient : '',
+                }}
+                onChange={e => setRecipient(e)}
+                name="recipient"
+              />
+            </div>
+            <div>
+              <span>Entregador</span>
+              <CustomAsyncSelect
+                isSearchable
+                defaultOptions
+                loadOptions={e => loadDeliverymen(e)}
+                defaultValue={{
+                  label:
+                    order && order.deliveryman ? order.deliveryman.name : '',
+                  value: order && order.deliveryman ? order.deliveryman : '',
+                }}
+                onChange={e => setDeliveryman(e)}
+                name="deliveryman"
+              />
+            </div>
+          </div>
+          <div className="product">
+            <span>Nome do produto</span>
+            <Input name="product" type="text" placeholder="Nome do produto" />
+          </div>
+        </Content>
+      </Form>
+    </Container>
+  );
+}
+
+OrdersForm.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
---
+++ frontend/src/pages/Orders/Form/styles.js
@@ -0,0 +1,123 @@
+import styled from 'styled-components';
+import AsyncSelect from 'react-select/async';
+
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  margin: 30px auto;
+  width: 1200px;
+`;
+
+export const Header = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  div.headerButtons {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 28px;
+    text-align: left;
+  }
+`;
+
+export const StyledButton = styled.button`
+  align-items: center;
+  background-color: #cccccc;
+  border-radius: 4px;
+  border: 0;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 8px 15px;
+  transition: background-color 300ms ease;
+
+  &:hover {
+    background-color: ${darken(0.07, '#cccccc')};
+  }
+
+  & + button {
+    background-color: #7d40e7;
+    margin-left: 16px;
+  }
+
+  & + button:hover {
+    background-color: ${darken(0.07, '#7D40E7')};
+  }
+
+  svg {
+    margin-right: 8px;
+  }
+`;
+
+export const Content = styled.div`
+  background-color: #fff;
+  border-radius: 4px;
+  margin-top: 20px;
+  padding: 35px 30px;
+
+  span {
+    font-weight: bold;
+    color: #444;
+    font-size: 14px;
+  }
+
+  div.searchInputs {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 30px;
+    align-items: center;
+
+    .css-yk16xz-control {
+      height: 45px;
+    }
+
+    .css-1pahdxg-control {
+      height: 45px;
+
+      &:hover {
+        box-shadow: none;
+      }
+    }
+  }
+
+  div.product {
+    margin-top: 20px;
+
+    input {
+      border-radius: 4px;
+      border: 1px solid #ccc;
+      color: #999;
+      font-size: 16px;
+      margin-top: 9px;
+      outline: 0;
+      padding: 12.5px 12px;
+      width: 100%;
+    }
+  }
+`;
+
+export const CustomAsyncSelect = styled(AsyncSelect)`
+  color: #999;
+  font-size: 16px;
+  margin-top: 9px;
+
+  .css-1hwfws3 {
+    padding: 10px;
+  }
+
+  .css-1uccc91-singleValue {
+    color: #999;
+    padding: 10px 0px;
+  }
+`;
